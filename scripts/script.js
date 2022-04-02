b1d68ee060d538262b44c53942db584bfb531093
NeYaroslav <91835506+NeYaroslav@users.noreply.github.com>
2022-04-02 10:49:38 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,16 @@ const burger = document.querySelector(".header__burger")
 burger.addEventListener("click", ()=>{
 	document.querySelector("body").classList.toggle("show_menu")
 })
+const links = document.querySelectorAll(".header__nav__menu__link a")
+for(let index = 0; index < links.length; index++){
+    if(document.querySelector("body").contains(".show_menu")) {
+        links[index].addEventListener("click", ()=>{
+	document.querySelector("body").classList.remove("show_menu")
+})
+    }
+}
+
+
 
 window.addEventListener("scroll", ()=> {
     if(!scrollY == 0) {
